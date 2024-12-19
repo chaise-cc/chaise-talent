e76ad4d32483593403e3726d785f2e1a1ae75c75
Mr. Paul Ishaili C. <paulishaili@gmail.com>
2024-12-19 17:45:09 +0100

    ...

--- src/app/(pages)/(dashboard)/layout.tsx
+++ src/app/(pages)/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { LayoutTransition } from "@/LayoutTransition";
 import DashboardHeader from "./_components/header";
 import DesktopSideBar from "./_components/sidebar/desktop.sidebar";
 import "./_styles/index.scss";
@@ -13,7 +14,15 @@ export default function DashboardLayout({
 
       <div className="md:ml-60 w-full">
         <DashboardHeader />
-        <main className="p-4 w-full">{children}</main>
+        <main className="p-4 w-full">
+          <LayoutTransition
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            {children}
+          </LayoutTransition>
+        </main>
       </div>
     </div>
   );
