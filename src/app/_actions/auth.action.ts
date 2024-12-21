f17c4fbf48b0059b7ed27e5497963d59baaf6692
Mr. Paul Ishaili C. <paulishaili@gmail.com>
2024-12-21 07:34:41 +0100

    ...

--- src/app/_actions/auth.action.ts
+++ src/app/_actions/auth.action.ts
@@ -11,7 +11,6 @@ const loginSchema = z.object({
     .string()
     .min(8, { message: "Password must be at least 8 characters" })
     .trim(),
-  activeRole: z.string().optional(), // Mark activeRole as optional
 });
 
 export async function login(prevState: unknown, formData: FormData) {
@@ -24,7 +23,7 @@ export async function login(prevState: unknown, formData: FormData) {
     };
   }
 
-  const { email, password, activeRole } = result.data;
+  const { email, password } = result.data;
 
   // Locate the user
   const user = users.find((u) => u.email === email && password === "12345678");
@@ -46,25 +45,14 @@ export async function login(prevState: unknown, formData: FormData) {
     };
   }
 
-  // Validate activeRole
-  const activeRoleFromForm = formData.get("activeRole");
-  const activeRoleString =
-    typeof activeRoleFromForm === "string" ? activeRoleFromForm : activeRole;
-
-  if (activeRoleString && !user.accounts.includes(activeRoleString)) {
-    return {
-      errors: {
-        email: ["Invalid account type selected"],
-      },
-    };
-  }
-
   // Default to the first account if activeRole is not specified
-  const roleToSet = activeRoleString || user.accounts[0];
+  const roleToSet = user.accounts[0];
 
   await createSession(user, roleToSet);
 
-  return redirect(`/dashboard`); // Direct to specific role dashboard
+  return roleToSet === "talent"
+    ? redirect(`/dashboard`)
+    : roleToSet === "client" && redirect(`/panel`); // Direct to specific role dashboard
 }
 
 export async function logout() {
