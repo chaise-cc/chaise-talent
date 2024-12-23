fd56e0d9f3169e751fabd5f8dcae2dfe24ba39f7
Mr. Paul Ishaili C. <paulishaili@gmail.com>
2024-12-23 05:27:46 +0100

    ... updated

--- src/app/(pages)/(onboarding)/(pages)/talent-onboarding/personal-info/page.tsx
+++ src/app/(pages)/(onboarding)/(pages)/talent-onboarding/personal-info/page.tsx
@@ -23,6 +23,9 @@ import {
 } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Add } from "iconsax-react";
+import Image from "next/image";
+import { useRef, useState } from "react";
 
 const TalentPersonalDetailsFormSchema = z.object({
   firstName: z.string().min(2, {
@@ -68,6 +71,38 @@ const TalentPersonalDetailsScreen = () => {
     code,
     ...details,
   }));
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [, setUploading] = useState(false);
+
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const handleFileInputClick = () => {
+    fileInputRef.current?.click();
+  };
+  async function uploadAvatar(event: React.ChangeEvent<HTMLInputElement>) {
+    try {
+      setUploading(true);
+
+      if (!event.target.files || event.target.files.length === 0) {
+        throw new Error("You must select an image to upload.");
+      }
+
+      //   const file = event.target.files[0];
+      //   const fileExt = file.name.split(".").pop();
+      //   const fileName = `${Math.random()}.${fileExt}`;
+      //   const filePath = `avatars/${fileName}`;
+    } finally {
+      setUploading(false);
+    }
+  }
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      const previewUrl = URL.createObjectURL(file);
+      setPreviewUrl(previewUrl);
+    }
+    uploadAvatar(event);
+  };
 
   return (
     <div className="container w-full py-6 md:py-8 !px-8 max-w-5xl mx-auto">
@@ -76,11 +111,9 @@ const TalentPersonalDetailsScreen = () => {
           <div className="flex flex-col gap-10">
             <div className="flex flex-col gap-1">
               <h1 className="text-2xl md:text-3xl mb-1 font-semibold">
-                Personal Information
-              </h1>
-              <h1 className="text-xl md:text-2xl mb-2 leading-none font-medium">
                 Tell us about yourself
               </h1>
+
               <p className="text-sm md:text-base">
                 We need your personal details, you can edit it before your
                 profile goes live.
@@ -88,6 +121,36 @@ const TalentPersonalDetailsScreen = () => {
             </div>
 
             <div className="flex flex-col gap-4 px-4 py-6 md:p-8 md:py-12 rounded-xl border">
+              <div className="talent-avatar mx-auto mb-10 h-32 w-32 md:h-44 md:w-44 border-2 relative border-black bg-gray-100 border-dotted rounded-full">
+                {previewUrl ? (
+                  <Image
+                    height={200}
+                    width={200}
+                    quality={100}
+                    src={previewUrl}
+                    alt="Avatar Preview"
+                    className="h-full w-full object-cover rounded-full"
+                  />
+                ) : (
+                  <div className="flex flex-col items-center justify-center h-full">
+                    <div
+                      className="absolute bottom-2 h-8 w-8 grid place-items-center right-2 upload-button z-50 bg-main-color-500 text-white rounded-full cursor-pointer"
+                      onClick={handleFileInputClick}
+                    >
+                      <Add color="black" size={20} />
+                    </div>
+                    <p>Upload photo</p>
+                  </div>
+                )}
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  style={{ display: "none" }}
+                  accept="image/*"
+                  onChange={handleFileChange}
+                />
+              </div>
+
               <div className="grid md:grid-cols-2 gap-4 ">
                 <FormField
                   control={form.control}
@@ -139,12 +202,17 @@ const TalentPersonalDetailsScreen = () => {
                         Gender
                       </FormLabel>
                       <FormControl>
-                        <Input
-                          className="text-base placeholder:text-base md:text-base py-6"
-                          placeholder="shadcn"
-                          type="date"
-                          {...field}
-                        />
+                        <Select {...field}>
+                          <SelectTrigger className="w-full py-6 text-base">
+                            <SelectValue placeholder="Choose your Gender" />
+                          </SelectTrigger>
+                          <SelectContent className="text-black font-medium !text-base">
+                            <SelectItem value="male">Male</SelectItem>
+                            <SelectItem value="female">Female</SelectItem>
+                            <SelectItem value="others">Others</SelectItem>
+                            <SelectItem value="none">Rather not say</SelectItem>
+                          </SelectContent>
+                        </Select>
                       </FormControl>
                       <FormMessage />
                     </FormItem>
