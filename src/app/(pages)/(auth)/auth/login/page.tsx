e7fc0248d7757d5279e0c4d4292a7056e7e8fda9
Mr. Paul Ishaili C. <paulishaili@gmail.com>
2024-12-25 10:35:26 +0100

    ...

--- src/app/(pages)/(auth)/auth/login/page.tsx
+++ src/app/(pages)/(auth)/auth/login/page.tsx
@@ -3,13 +3,24 @@
 import Link from "next/link";
 import FormHeader from "@/components/custom/FormHeader";
 import LoginForm from "../../_components/Login.form";
+import { Suspense } from "react";
+import { Loader2 } from "lucide-react";
 
 const LoginPage = () => {
   return (
     <div className="w-full flex flex-col items-center">
       <FormHeader title="Welcome back!" description="Login to your account" />
 
-      <LoginForm />
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center space-x-2">
+            <Loader2 size={24} className="animate-spin" />
+            <span>Loading...</span>
+          </div>
+        }
+      >
+        <LoginForm />
+      </Suspense>
 
       <div className="grid place-items-center text-sm gap-2 my-4 ">
         <p>
