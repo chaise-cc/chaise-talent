db50d0605b6a76b8cb15a53c5bc78e751c77e341
Mr. Paul Ishaili C. <paulishaili@gmail.com>
2024-12-26 13:56:44 +0100

    x

--- src/app/(pages)/verify-email/page.tsx
+++ src/app/(pages)/verify-email/page.tsx
@@ -1,12 +1,14 @@
 // File: app/verify-email/page.tsx
 
+import { Suspense } from "react";
 import VerifyEmailClient from "./verify-email-client";
-// import VerifyEmailClient from "@/components/VerifyEmailClient"; // Import the client component
 
 export default async function VerifyEmailPage() {
   return (
     <div className="max-w-lg mx-auto w-full py-4 text-center">
-      <VerifyEmailClient /> {/* Render the client component */}
+      <Suspense fallback="loading...">
+        <VerifyEmailClient /> {/* Render the client component */}
+      </Suspense>
     </div>
   );
 }
