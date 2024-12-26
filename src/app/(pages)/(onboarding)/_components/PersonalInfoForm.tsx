6d93c1471b98669c352e4e855321c2cfab27dab6
Mr. Paul Ishaili C. <paulishaili@gmail.com>
2024-12-26 21:37:27 +0100

    ..

--- src/app/(pages)/(onboarding)/_components/PersonalInfoForm.tsx
+++ src/app/(pages)/(onboarding)/_components/PersonalInfoForm.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Button } from "@/components/ui/button"; // Assuming you have custom styled components
-import { ArrowRight } from "iconsax-react";
+import { Add, ArrowRight, Edit2 } from "iconsax-react";
 import { updateUser } from "@/app/_actions/updateUser.action";
 import countries from "@/data/countries.json";
 import languagesData from "@/data/languages.json";
 import Image from "next/image";
 import { logout } from "@/app/_actions/auth.action";
 import { toast } from "sonner";
+import { Plus } from "lucide-react";
 
 const languages = Object.entries(languagesData).map(([code, details]) => ({
   code,
@@ -38,7 +39,11 @@ export default function PersonalInfoForm({ user }: TalentPersonalInfoProps) {
     country: user.country || "",
     language: user.language || "",
   });
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
+  const [previewUrl, setPreviewUrl] = useState<string | undefined>(
+    formData.avatarUrl as string
+  );
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [submitMessage, setSubmitMessage] = useState("");
 
@@ -53,11 +58,28 @@ export default function PersonalInfoForm({ user }: TalentPersonalInfoProps) {
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleAvatarChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0]; // Get the first selected file
-    if (file) {
-      setFormData((prev) => ({ ...prev, avatarUrl: file }));
+  const handleAvatarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      const newPreviewUrl = URL.createObjectURL(file);
+
+      // Revoke the previous object URL to prevent memory leaks
+      if (previewUrl) URL.revokeObjectURL(previewUrl);
+
+      setPreviewUrl(newPreviewUrl);
+      if (file) {
+        setFormData((prev) => ({ ...prev, avatarUrl: file }));
+      }
     }
+  };
+
+  const handleFileInputClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const removePreview = () => {
+    if (previewUrl) URL.revokeObjectURL(previewUrl);
+    setPreviewUrl(undefined);
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -97,30 +119,71 @@ export default function PersonalInfoForm({ user }: TalentPersonalInfoProps) {
           </div>
 
           <div className="flex flex-col gap-4 px-4 py-6 md:gap-6 md:p-8 md:py-12 w-full rounded-xl border">
-            <div>
-              <label htmlFor="avatarUrl" className="block text-sm font-medium">
-                Avatar
-              </label>
+            <div className="talent-avatar mx-auto mb-2 h-32 w-32 md:h-44 md:w-44 border-2 relative border-black bg-gray-100 border-dotted rounded-full">
+              {previewUrl ? (
+                <div className="relative h-full">
+                  <Image
+                    height={200}
+                    width={200}
+                    src={previewUrl}
+                    alt="Avatar Preview"
+                    className="h-full w-full object-cover rounded-full"
+                  />
+                  <div
+                    className="absolute bottom-1 md:bottom-2 h-7 md:h-8 w-7 md:w-8 grid place-items-center right-1 md:right-2 upload-button z-50 bg-main-color-500 text-white rounded-full cursor-pointer"
+                    onClick={handleFileInputClick}
+                    aria-label="Edit Avatar"
+                  >
+                    <Edit2 color="black" size={14} />
+                  </div>
+                </div>
+              ) : (
+                <div className="flex flex-col items-center justify-center h-full">
+                  <div
+                    className="absolute bottom-1 md:bottom-2 h-7 w-7 md:h-8 md:w-8 grid place-items-center right-1 md:right-2 upload-button z-50 bg-main-color-500 text-white rounded-full cursor-pointer"
+                    onClick={handleFileInputClick}
+                    aria-label="Upload Avatar"
+                  >
+                    <Plus className="text-base md:text-xl" size={20} />
+                  </div>
+                  <p className="font-varela text-sm md:text-base font-medium">
+                    Upload photo
+                  </p>
+                </div>
+              )}
               <input
                 type="file"
+                ref={fileInputRef}
                 id="avatarUrl"
                 name="avatarUrl"
+                style={{ display: "none" }}
                 accept="image/*"
                 onChange={handleAvatarChange}
-                className="mt-1"
               />
-              {/* Optionally, show the current avatar as an image if it exists */}
-              {formData.avatarUrl && typeof formData.avatarUrl === "string" && (
-                <Image
-                  src={formData.avatarUrl}
-                  alt="Current Avatar"
-                  height={100}
-                  width={100}
-                  quality={100}
-                  className="mt-4 w-20 h-20 rounded-full"
-                />
-              )}
             </div>
+
+            {previewUrl && (
+              <div className="flex w-max mb-4 mx-auto gap-4">
+                <Button
+                  type="button"
+                  className="flex bg-red-500 hover:bg-red-100 border border-transparent hover:border-red-500 pl-2 pr-4 text-white hover:text-black py-4 gap-1 font-semibold text-sm w-max leading-none"
+                  onClick={removePreview}
+                  aria-label="Remove Avatar"
+                >
+                  <Add className="rotate-45 text-xl" size={20} color="black" />{" "}
+                  Remove
+                </Button>
+
+                <Button
+                  type="button"
+                  className="flex bg-green-500 text-white hover:bg-green-100 border border-transparent hover:border-green-500 hover:text-black py-4 font-semibold text-sm mx-auto w-max mb-4 pr-3 leading-none"
+                  onClick={handleFileInputClick}
+                  aria-label="Upload Another Avatar"
+                >
+                  Upload another
+                </Button>
+              </div>
+            )}
 
             <div className="grid md:grid-cols-2 gap-4">
               <div>
