95ca2936746cf9475e0675d4224bcf4cb80ed29e
Mr. Paul Ishaili C. <paulishaili@gmail.com>
2024-12-30 15:35:38 +0100

    ...

--- src/app/(pages)/(onboarding)/_components/PersonalInfoForm.tsx
+++ src/app/(pages)/(onboarding)/_components/PersonalInfoForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRef, useState } from "react";
-import { Button } from "@/components/ui/button"; // Assuming you have custom styled components
+import { Button } from "@/components/ui/button";
 import { ArrowRight } from "iconsax-react";
 import { updateUser } from "@/app/_actions/updateUser.action";
 import countries from "@/data/countries.json";
@@ -31,7 +31,7 @@ type TalentPersonalInfoProps = {
 
 export default function PersonalInfoForm({ user }: TalentPersonalInfoProps) {
   const [formData, setFormData] = useState({
-    avatarUrl: user.avatar || "", // Assuming this is a URL or null if not provided
+    avatarUrl: user.avatar || "",
     firstname: user.firstname || "",
     lastname: user.lastname || "",
     gender: user.gender || "",
@@ -39,17 +39,12 @@ export default function PersonalInfoForm({ user }: TalentPersonalInfoProps) {
     country: user.country || "",
     language: user.language || "",
   });
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
   const [previewUrl, setPreviewUrl] = useState<string | undefined>(
     formData.avatarUrl as string
   );
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [submitMessage, setSubmitMessage] = useState("");
-
-  const handleLogout = async () => {
-    await logout();
-  };
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
@@ -58,28 +53,19 @@ export default function PersonalInfoForm({ user }: TalentPersonalInfoProps) {
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleAvatarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files.length > 0) {
-      const file = event.target.files[0];
+  const handleAvatarChange = (file: File | null) => {
+    if (file) {
       const newPreviewUrl = URL.createObjectURL(file);
-
-      // Revoke the previous object URL to prevent memory leaks
       if (previewUrl) URL.revokeObjectURL(previewUrl);
-
       setPreviewUrl(newPreviewUrl);
-      if (file) {
-        setFormData((prev) => ({ ...prev, avatarUrl: file }));
-      }
+      setFormData((prev) => ({ ...prev, avatarUrl: file }));
     }
-  };
-
-  const handleFileInputClick = () => {
-    fileInputRef.current?.click();
   };
 
   const removePreview = () => {
     if (previewUrl) URL.revokeObjectURL(previewUrl);
     setPreviewUrl(undefined);
+    setFormData((prev) => ({ ...prev, avatarUrl: "" }));
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -87,19 +73,17 @@ export default function PersonalInfoForm({ user }: TalentPersonalInfoProps) {
     setIsSubmitting(true);
 
     try {
-      // Call the server action directly with the file object
       await updateUser(
         user.id,
         formData,
-        formData.avatarUrl instanceof File ? formData.avatarUrl : null // Ensure avatarUrl is a File when submitting
+        formData.avatarUrl instanceof File ? formData.avatarUrl : null
       );
-      setSubmitMessage("Profile updated successfully!");
-      toast.success(submitMessage);
-      handleLogout();
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      toast.success("Profile updated successfully!");
+      await logout();
     } catch (error) {
-      setSubmitMessage("Error updating profile. Please try again.");
-      toast.error(submitMessage);
+      console.log(error);
+
+      toast.error("Error updating profile. Please try again.");
     } finally {
       setIsSubmitting(false);
     }
@@ -109,33 +93,20 @@ export default function PersonalInfoForm({ user }: TalentPersonalInfoProps) {
     <div className="container w-full max-w-5xl pb-16">
       <form onSubmit={handleSubmit} className="space-y-8 w-full">
         <div className="flex flex-col w-full gap-10">
-          <div className="flex flex-col gap-1">
-            <h1 className="text-2xl md:text-3xl mb-1 font-semibold">
-              Update your profile
-            </h1>
-            <p className="text-sm md:text-base">
-              Let&apos;s know more about you. These will appear on your profile.
-            </p>
-          </div>
+          <h1 className="text-2xl md:text-3xl mb-1 font-semibold">
+            Update your profile
+          </h1>
+          <p className="text-sm md:text-base">
+            Let&apos;s know more about you. These will appear on your profile.
+          </p>
 
           <div className="flex flex-col gap-4 px-4 py-6 md:gap-6 md:p-8 md:py-12 w-full rounded-xl border">
-            <div className="flex flex-col gap-4 justify-center">
-              <AvatarInput
-                previewUrl={previewUrl}
-                onFileChange={handleFileInputClick}
-                onRemove={removePreview}
-                fileInputRef={fileInputRef}
-              />
-              <input
-                type="file"
-                ref={fileInputRef}
-                id="avatarUrl"
-                name="avatarUrl"
-                style={{ display: "none" }}
-                accept="image/*"
-                onChange={handleAvatarChange}
-              />
-            </div>
+            <AvatarInput
+              previewUrl={previewUrl}
+              onFileChange={handleAvatarChange}
+              onRemove={removePreview}
+              fileInputRef={fileInputRef}
+            />
 
             <div className="grid md:grid-cols-2 gap-4">
               <div>
@@ -147,7 +118,6 @@ export default function PersonalInfoForm({ user }: TalentPersonalInfoProps) {
                   name="firstname"
                   value={formData.firstname}
                   disabled
-                  onChange={handleChange}
                   className="mt-1 py-4 border px-4 text-base rounded-xl w-full"
                 />
               </div>
@@ -161,7 +131,6 @@ export default function PersonalInfoForm({ user }: TalentPersonalInfoProps) {
                   name="lastname"
                   value={formData.lastname}
                   disabled
-                  onChange={handleChange}
                   className="mt-1 py-4 border px-4 text-base rounded-xl w-full"
                 />
               </div>
@@ -211,10 +180,7 @@ export default function PersonalInfoForm({ user }: TalentPersonalInfoProps) {
               >
                 <option value="">Select Country</option>
                 {countries.map((country) => (
-                  <option
-                    key={country.name}
-                    value={country.name.toLocaleLowerCase()}
-                  >
+                  <option key={country.name} value={country.name.toLowerCase()}>
                     {country.name}
                   </option>
                 ))}
@@ -233,33 +199,28 @@ export default function PersonalInfoForm({ user }: TalentPersonalInfoProps) {
               >
                 <option value="">Select Language</option>
                 {languages.map((language) => (
-                  <option
-                    key={language.name.toLocaleLowerCase()}
-                    value={language.name.toLocaleLowerCase()}
-                  >
+                  <option key={language.code} value={language.code}>
                     {language.name}
                   </option>
                 ))}
               </select>
             </div>
 
-            <div className="flex w-full items-center justify-between">
-              <Button
-                type="submit"
-                className="py-6 bg-main-color-500 mx-auto text-black hover:bg-main-color-300 mt-4 font-semibold px-8 flex items-center gap-2"
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? (
-                  <>
-                    Updating... <Loader2 size={18} className="animate-spin" />
-                  </>
-                ) : (
-                  <>
-                    Update <ArrowRight size={20} color="black" />
-                  </>
-                )}
-              </Button>
-            </div>
+            <Button
+              type="submit"
+              className="py-6 bg-main-color-500 mx-auto text-black hover:bg-main-color-300 mt-4 font-semibold px-8 flex items-center gap-2"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? (
+                <>
+                  Updating... <Loader2 size={18} className="animate-spin" />
+                </>
+              ) : (
+                <>
+                  Update <ArrowRight size={20} color="black" />
+                </>
+              )}
+            </Button>
           </div>
         </div>
       </form>
