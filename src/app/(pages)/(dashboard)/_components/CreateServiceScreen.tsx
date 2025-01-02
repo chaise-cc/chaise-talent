368a4db2e26e4edfdff1ec7915310f06d369ed62
Mr. Paul Ishaili C. <paulishaili@gmail.com>
2025-01-02 11:18:01 +0100

    ..

---
+++ src/app/(pages)/(dashboard)/_components/CreateServiceScreen.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function CreateServiceScreen() {
+  return (
+    <div className="w-full max-w-4xl mx-auto shadow-sm rounded-xl">
+      {/*  */}
+      <div className="flex justify-between items-center">
+        <h2></h2>
+        <p>Step 1</p>
+        <div className="">
+          <Button>ds</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
