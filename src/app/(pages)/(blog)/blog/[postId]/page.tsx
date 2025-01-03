8e01f053e844cc87c4fcd0ddca1d8ac08e41512d
Mr. Paul Ishaili C. <paulishaili@gmail.com>
2025-01-03 18:42:46 +0100

    ...

--- src/app/(pages)/(blog)/blog/[postId]/page.tsx
+++ src/app/(pages)/(blog)/blog/[postId]/page.tsx
@@ -54,7 +54,8 @@ export default async function BlogPostPage(props: {
       </section>
 
       <section
-        className="container prose py-4 md:py-8 w-full !max-w-6xl first-letter:text-3xl first-letter:font-bold mx-auto"
+        style={{ fontSize: "18px" }}
+        className="container prose py-4 md:py-8 w-full !max-w-6xl mx-auto"
         dangerouslySetInnerHTML={{ __html: post.content }}
       ></section>
     </MainLayout>
