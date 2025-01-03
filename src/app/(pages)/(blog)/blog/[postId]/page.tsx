0cbb15fdbc8b38265542b1cfcc9db7713c54ac89
Mr. Paul Ishaili C. <paulishaili@gmail.com>
2025-01-03 18:19:46 +0100

    ... added tailwind typography

--- src/app/(pages)/(blog)/blog/[postId]/page.tsx
+++ src/app/(pages)/(blog)/blog/[postId]/page.tsx
@@ -54,7 +54,7 @@ export default async function BlogPostPage(props: {
       </section>
 
       <section
-        className="container py-4 md:py-8 w-full !max-w-6xl first-letter:text-3xl first-letter:font-bold mx-auto"
+        className="container prose py-4 md:py-8 w-full !max-w-6xl first-letter:text-3xl first-letter:font-bold mx-auto"
         dangerouslySetInnerHTML={{ __html: post.content }}
       ></section>
     </MainLayout>
